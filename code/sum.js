7486938f5c5877af1849b449b87676c5d1a77848
yexiaolong <771388996@qq.com>
2021-11-24 13:37:03 +0800

    init

---
+++ code/sum.js
@@ -0,0 +1,9 @@
+/**
+ * User: yexiaolong/771388996@qq.com
+ * Date: 2021/11/24
+ * Time: 12:48 下午
+ */
+function sum(a, b) {
+    return a + b
+}
+export default sum
---
+++ tests/unit/sum.spec.js
@@ -0,0 +1,11 @@
+/**
+ * User: yexiaolong/771388996@qq.com
+ * Date: 2021/11/24
+ * Time: 12:49 下午
+ */
+import sum from '../../code/sum'
+describe('HelloWorld.vue', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1,2)).toBe(3)
+    })
+})
